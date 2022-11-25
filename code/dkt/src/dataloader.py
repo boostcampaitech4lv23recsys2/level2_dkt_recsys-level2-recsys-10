1aae67ab245ed49bc5d0bac103cfce5a19bed51c
kuuneeee <rjsdud0586@gmail.com>
2022-11-25 21:44:42 +0900

    feat: Modify Preprocess modify __preprocessing, load_data_from_file and DKTDataset

--- code/dkt/src/dataloader.py
+++ code/dkt/src/dataloader.py
@@ -2,6 +2,7 @@ import os
 import random
 import time
 from datetime import datetime
+from easydict import EasyDict
 
 import numpy as np
 import pandas as pd
@@ -41,15 +42,11 @@ class Preprocess:
         np.save(le_path, encoder.classes_)
 
     def __preprocessing(self, df, is_train=True):
-        # cate_cols = ["assessmentItemID", "testId", "KnowledgeTag"]
-        ############################
-        cate_cols = ["assessmentItemID", "testId", "KnowledgeTag", 'test_question']
-        ############################
 
         if not os.path.exists(self.args.asset_dir):
             os.makedirs(self.args.asset_dir)
 
-        for col in cate_cols:
+        for col in self.args.cate_feats:
 
             le = LabelEncoder()
             if is_train:
@@ -70,14 +67,6 @@ class Preprocess:
             test = le.transform(df[col])
             df[col] = test
 
-        def convert_time(s):
-            timestamp = time.mktime(
-                datetime.strptime(s, "%Y-%m-%d %H:%M:%S").timetuple()
-            )
-            return int(timestamp)
-
-        df["Timestamp"] = df["Timestamp"].apply(convert_time)
-
         return df
 
     def __feature_engineering(self, df):
@@ -87,64 +76,46 @@ class Preprocess:
     def load_data_from_file(self, file_name, is_train=True):
         csv_file_path = os.path.join(self.args.data_dir, file_name)
         df = pd.read_csv(csv_file_path)  # , nrows=100000)
+
+        if is_train:
+            df = df[df["grade"] == 1]
+        else:
+            df = df[(df['answerCode']==-1) | (df['answerCode']==0)]
+
         df = self.__feature_engineering(df)
         df = self.__preprocessing(df, is_train)
 
         # 추후 feature를 embedding할 시에 embedding_layer의 input 크기를 결정할때 사용
+        self.args.n_embeddings = EasyDict()
 
-        self.args.n_questions = len(
-            np.load(os.path.join(self.args.asset_dir, "assessmentItemID_classes.npy"))
-        )
-        self.args.n_test = len(
-            np.load(os.path.join(self.args.asset_dir, "testId_classes.npy"))
-        )
-        self.args.n_tag = len(
-            np.load(os.path.join(self.args.asset_dir, "KnowledgeTag_classes.npy"))
-        )
-
-        ############################
-        # self.args.n_month = len(
-        #     np.load(os.path.join(self.args.asset_dir, "month_classes.npy"))
-        # )
-        # self.args.n_category_2 = len(
-        #     np.load(os.path.join(self.args.asset_dir, "category_2_classes.npy"))
-        # )
-        # self.args.n_category_difficulty = len(
-        #     np.load(os.path.join(self.args.asset_dir, "category_difficulty_classes.npy"))
-        # )
-        # self.args.n_test_paper = len(
-        #     np.load(os.path.join(self.args.asset_dir, "test_paper_classes.npy"))
-        # )
-        self.args.n_test_question = len(
-            np.load(os.path.join(self.args.asset_dir, "test_question_classes.npy"))
-        )
-        ############################
-
+        print("\nload  *_classes.npy")
+        for col_name in self.args.cate_feats:
+            self.args.n_embeddings[col_name] = len(
+                np.load(os.path.join(self.args.asset_dir, col_name + "_classes.npy"))
+            )
+        
         df = df.sort_values(by=["userID", "Timestamp"], axis=0)
 
-        # columns = ["userID", "assessmentItemID", "testId", "answerCode", "KnowledgeTag"]
-        ############################
-        columns = ["userID", "assessmentItemID", "testId", "answerCode", "KnowledgeTag", 'test_question']
-        ############################
+        columns = [i for i in list(df) if i not in ["Timestamp", "dataset"]]
+
         group = (
             df[columns]
             .groupby("userID")
-            .apply(
-                lambda r: (
-                    r["testId"].values,
-                    r["assessmentItemID"].values,
-                    r["KnowledgeTag"].values,
-                    r["answerCode"].values,
-                    ############################
-                    # r["month"].values,
-                    # r["category_2"].values,
-                    # r["category_difficulty"].values,
-                    # r["test_paper"].values,
-                    r["test_question"].values,
-                    ############################
-                )
-            )
+            .apply(lambda r: tuple(r[col].values for col in columns))
         )
+
+        # columns position
+        self.args.columns = {col_name: idx for idx, col_name in enumerate(columns)}
+
+        # category feature location
+        self.args.cate_loc = {
+            col: i for i, col in enumerate(columns) if col in self.args.cate_feats
+        }
+
+        # category feature location
+        self.args.conti_loc = {
+            col: i for i, col in enumerate(columns) if col in self.args.conti_feats
+        }
 
         return group.values
 
@@ -166,35 +137,29 @@ class DKTDataset(torch.utils.data.Dataset):
         # 각 data의 sequence length
         seq_len = len(row[0])
 
-
-
-        # test, question, tag, correct = row[0], row[1], row[2], row[3]
-
-        # cate_cols = [test, question, tag, correct]
-        
-        ############################
-        test, question, tag, correct, test_question = row[0], row[1], row[2], row[3], row[4]
-
-        cate_cols = [test, question, tag, correct, test_question]
-        ############################
+        feat_cols = list(row)       # feat_cols / conti + cate
 
         # max seq len을 고려하여서 이보다 길면 자르고 아닐 경우 그대로 냅둔다
         if seq_len > self.args.max_seq_len:
-            for i, col in enumerate(cate_cols):
-                cate_cols[i] = col[-self.args.max_seq_len :]
+            for i, col in enumerate(feat_cols):
+                feat_cols[i] = col[-self.args.max_seq_len :]
+
             mask = np.ones(self.args.max_seq_len, dtype=np.int16)
         else:
             mask = np.zeros(self.args.max_seq_len, dtype=np.int16)
             mask[-seq_len:] = 1
 
         # mask도 columns 목록에 포함시킴
-        cate_cols.append(mask)
+        feat_cols.append(mask)
 
         # np.array -> torch.tensor 형변환
-        for i, col in enumerate(cate_cols):
-            cate_cols[i] = torch.tensor(col)
+        for i, col in enumerate(feat_cols):
+            if i in self.args.conti_loc.values():  # continus col index
+                feat_cols[i] = torch.FloatTensor(col)
+            else:
+                feat_cols[i] = torch.tensor(col)
 
-        return cate_cols
+        return feat_cols
 
     def __len__(self):
         return len(self.data)
@@ -272,7 +237,7 @@ def slidding_window(data, args):
                 # window로 잘린 데이터를 모으는 리스트
                 window_data = []
                 for col in row:
-                    window_data.append(col[window_i*stride:window_i*stride + window_size])
+                    window_data.append(col[ window_i * stride : window_i * stride + window_size])
 
                 # Shuffle
                 # 마지막 데이터의 경우 shuffle을 하지 않는다
@@ -289,7 +254,6 @@ def slidding_window(data, args):
                 for col in row:
                     window_data.append(col[-window_size:])
                 augmented_datas.append(tuple(window_data))
-
 
     return augmented_datas
 
