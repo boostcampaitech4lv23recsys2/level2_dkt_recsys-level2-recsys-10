85c3f169bb3dd7e2234db32e70a43ee16b21f708
kuuneeee <rjsdud0586@gmail.com>
2022-11-29 18:18:20 +0900

    feat: Modify Loss function Change BCEWithLogitsLoss to BCELoss

--- code/dkt/src/criterion.py
+++ code/dkt/src/criterion.py
@@ -2,5 +2,6 @@ import torch.nn as nn
 
 
 def get_criterion(pred, target):
-    loss = nn.BCEWithLogitsLoss(reduction="none")
+    # loss = nn.BCEWithLogitsLoss(reduction="none")
+    loss = nn.BCELoss(reduction="none")
     return loss(pred, target)
