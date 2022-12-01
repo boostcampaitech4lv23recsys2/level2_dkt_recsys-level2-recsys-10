fc880aa3832f6844a62863a13d7243c12aec97b0
hyunho <hyunho656@gmail.com>
2022-12-01 10:52:38 +0000

    feat: Add optuna code in new lgbm

--- code/lgbm/lgbm_baseline.py
+++ code/lgbm/lgbm_baseline.py
@@ -14,23 +14,82 @@ import argparse
 
 def feature_engineering(df):
     
-    #유저별 시퀀스를 고려하기 위해 아래와 같이 정렬
-    df.sort_values(by=['userID','Timestamp'], inplace=True)
     
-    #유저들의 문제 풀이수, 정답 수, 정답률을 시간순으로 누적해서 계산
-    df['user_correct_answer'] = df.groupby('userID')['answerCode'].transform(lambda x: x.cumsum().shift(1))
-    df['user_total_answer'] = df.groupby('userID')['answerCode'].cumcount()
-    df['user_acc'] = df['user_correct_answer']/df['user_total_answer']
+    ## 유저별 시퀀스를 고려하기 위해 아래와 같이 정렬
+    df.sort_values(by=['userID','Timestamp'], inplace=True)
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+    
+    ## 문제 푼 시간 재정의
+    # 같은 문제 몇번째 푸는지
+    df['same_item_cnt'] = df.groupby(['userID', 'assessmentItemID']).cumcount() + 1
+    
+    # elapsed
+    # total['Timestamp'] = pd.to_datetime(total['Timestamp'])
+    # # total['month'] = total['Timestamp'].dt.month
+    # total['hour'] = total['Timestamp'].dt.hour
+    # diff = total.loc[:, ['userID', 'Timestamp']].groupby('userID').diff().fillna(pd.Timedelta(seconds=0))
+    # diff = diff.fillna(pd.Timedelta(seconds=0))
+    # diff = diff['Timestamp'].apply(lambda x: x.total_seconds())
+    # total['elapsed'] = diff
+    
+    # 유저, test, same_item_cnt 구분했을 때 문제 푸는데 걸린 시간 > shift, fillna x
+    diff_shift = df.loc[:, ['userID', 'testId', 'Timestamp', 'same_item_cnt']].groupby(['userID', 'testId', 'same_item_cnt']).diff().shift(-1)
+    diff_shift = diff_shift['Timestamp'].apply(lambda x: x.total_seconds())
+    df['solved_time_shift'] = diff_shift
+    # total['solved_time_shift'] = total.groupby(['userID', 'testId', 'same_item_cnt'])['solved_time_shift'].apply(lambda x:x.fillna(x.mean()))
+    
+    # 1. agg 값 구하기
+    ## 1-1. 유저/문제/시험지/태그별 평균 정답률
+    df['user_avg'] = df.groupby('userID')['answerCode'].transform('mean')
+    df['item_avg'] = df.groupby('assessmentItemID')['answerCode'].transform('mean')
+    df['test_avg'] = df.groupby('testId')['answerCode'].transform('mean')
+    df['tag_avg'] = df.groupby('KnowledgeTag')['answerCode'].transform('mean')
+    df['user_avg_bytest'] = df.groupby(['userID','testId'])['answerCode'].transform('mean')
 
-    # testId와 KnowledgeTag의 전체 정답률은 한번에 계산
-    # 아래 데이터는 제출용 데이터셋에 대해서도 재사용
-    correct_t = df.groupby(['testId'])['answerCode'].agg(['mean', 'sum'])
-    correct_t.columns = ["test_mean", 'test_sum']
-    correct_k = df.groupby(['KnowledgeTag'])['answerCode'].agg(['mean', 'sum'])
-    correct_k.columns = ["tag_mean", 'tag_sum']
-
-    df = pd.merge(df, correct_t, on=['testId'], how="left")
-    df = pd.merge(df, correct_k, on=['KnowledgeTag'], how="left")
+    ## 1-2. 유저/문제/시험지별 평균 풀이시간
+    df['user_time_avg'] = df.groupby('userID')['solved_time_shift'].transform('mean')
+    df['item_time_avg'] = df.groupby('assessmentItemID')['solved_time_shift'].transform('mean')
+    df['test_time_avg'] = df.groupby('testId')['solved_time_shift'].transform('mean')
+    df['tag_time_avg'] = df.groupby('KnowledgeTag')['solved_time_shift'].transform('mean')
+    
+    # 맞은 사람의 문제별 평균 풀이시간
+    df = df.set_index('assessmentItemID')
+    df['Item_mean_solved_time'] = df[df['answerCode'] == 1].groupby('assessmentItemID')['solved_time_shift'].mean()
+    df = df.reset_index(drop = False)
+     
+    # 유저/문제/시험지/태그별 표준편차
+    df['user_std'] = df.groupby('userID')['answerCode'].transform('std')
+    df['item_std'] = df.groupby('assessmentItemID')['answerCode'].transform('std')
+    df['test_std'] = df.groupby('testId')['answerCode'].transform('std')
+    df['tag_std'] = df.groupby('KnowledgeTag')['answerCode'].transform('std')
+    
+    ## 1-3. 현재 유저의 해당 문제지 평균 정답률/풀이시간
+    df['user_current_avg'] = df.groupby(['userID', 'testId', 'same_item_cnt'])['answerCode'].transform('mean')
+    df['user_current_time_avg'] = df.groupby(['userID', 'testId', 'same_item_cnt'])['solved_time_shift'].transform('mean')
+    
+    # 2. 컬럼 추가
+    df['hour'] = df['Timestamp'].dt.hour
+    df['hour'] = df['hour'].astype('category')
+    
+    df['month'] = df['Timestamp'].dt.month
+    df['month'] = df['month'].astype('category')
+    
+    ## 문제 번호 추가
+    df['item_num'] = df['assessmentItemID'].str[7:]
+    df['item_num'] = df['item_num'].astype('category')
+    # in2idx = {v:k for k,v in enumerate(df['item_num'].unique())}
+    # df['item_num'] = df['item_num'].map(in2idx)
+    
+    ## 문제 푼 순서 추가 > 상대적 순서?
+    df['item_seq'] = df.groupby(['userID', 'testId', 'same_item_cnt']).cumcount() +1
+    df['item_seq'] = df['item_seq'].astype('category')
+    # df['item_seq'] = df['item_seq'] - df['item_num'].astype(int)
+    # df['item_num'] = df['item_num'].astype('category')
+    # is2idx = {v:k for k,v in enumerate(df['item_seq'].unique())}
+    
+    df['Bigcat'] = df['testId'].str[2]
+    df['Bigcat'] = df['Bigcat'].astype('category')
+    df['Bigcat_avg'] = df.groupby('Bigcat')['answerCode'].transform('mean')
     
     return df
 
@@ -77,8 +136,10 @@ def main(args):
     train, test = custom_train_test_split(df, ratio=args.train_ratio)
 
     # 사용할 Feature 설정
-    FEATS = ['KnowledgeTag', 'user_correct_answer', 'user_total_answer', 
-            'user_acc', 'test_mean', 'test_sum', 'tag_mean','tag_sum']
+    # FEATS = ['KnowledgeTag', 'user_correct_answer', 'user_total_answer', 
+    #         'user_acc', 'test_mean', 'test_sum', 'tag_mean','tag_sum']
+    FEATS = ['KnowledgeTag', 'same_item_cnt', 'user_avg', 'item_avg', 'test_avg', 'tag_avg', 'user_time_avg', 'item_time_avg',
+       'test_time_avg', 'tag_time_avg', 'user_current_avg', 'user_current_time_avg', 'hour', 'item_num', 'Bigcat','Bigcat_avg']
 
     # X, y 값 분리
     y_train = train['answerCode']
